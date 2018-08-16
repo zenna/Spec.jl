a1646ab94f94e4c905cd47365d9ea86dbbe9a4ff
Zenna Tavares <zennatavares@gmail.com>
2018-08-16 13:28:11 -0400

    rm duplicates

--- src/Spec.jl
+++ src/Spec.jl
@@ -1,4 +1,3 @@
-__precompile__(false)
 "Testing and specification"
 module Spec
 
--- src/pre.jl
+++ src/pre.jl
@@ -1,11 +1,3 @@
-"Most recently created method of generic function `func`"
-newestmethod(func::Function) = sort(methods(func).ms, by=m->m.min_world)[end]
-
-# macro testing(args...)
-#   @show [typeof(arg) for arg in args]
-# end
-#
-# @testing x + 1 ArgumentError("msg") x / 3
 PRE_CHECKING_ON = true
 
 "Check preconditions"
